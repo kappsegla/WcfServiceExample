f57c89ce2db244a310a25caaae5041d3eb4514f6
Martin Blomberg <martin.blomberg@outlook.com>
2017-12-12 11:00:32 +0100

    Eventlog added

--- WindowsServiceHost/HelloWindowsService.cs
+++ WindowsServiceHost/HelloWindowsService.cs
@@ -14,22 +14,35 @@ namespace WindowsServiceHost
     public partial class HelloWindowsService : ServiceBase
     {
         ServiceHost host;
+        EventLog eventlog;
 
         public HelloWindowsService()
         {
             InitializeComponent();
+            eventlog = new EventLog();
+            //this.AutoLog = false;
+            
+            eventlog.Source = "HelloWindowsService";
+            eventlog.Log = "";
         }
 
         protected override void OnStart(string[] args)
         {
+            eventlog.WriteEntry("This is a test log message from OnStart");
             host = new ServiceHost(typeof(WcfServiceLibrary1.Service1));
             host.Open();
         }
-
+       
         protected override void OnStop()
         {
             if(host != null)
              host.Close();
         }
+
+        protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
+        {
+            return base.OnPowerEvent(powerStatus);
+        }
+
     }
 }
